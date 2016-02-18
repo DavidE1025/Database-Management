4c4a72bddd1bf09e0befe6b5a031a3c5fbb1cd86
DavidE1025 <DavidEmory1025@gmail.com>
2016-02-17 16:40:08 -0800

    Comments
    The questions for lab 4

---
+++ Lab 4.sql
@@ -0,0 +1,12 @@
+--David Emory
+--2/17/2016
+--Lab 4 Sub-Queries
+
+--1. Get the cities of agents booking an order for a customer whos cid is 'c002'
+--2. Get the ids of products ordered through any agent who takes atleast one order from a customer in Dallas, sorted by pid from highest to lowest 
+--3. Get the ids and names of customers who di not place an order through agent a01
+--4. Get the ids of customers who ordered both product p01 and p07
+--5. Get the ids of products not ordered by any customers who placed any order through agent a07 in pid order from highest to lowest 
+--6. Get the name, discounts, and city for all customers who placed orders through agents in London or new York.
+--7. Get all customers who have the same discount as that of any customers in Dallas or London 
+--8. Lot of typing (Dave is cool) 
