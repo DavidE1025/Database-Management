dd775674ac07a9edf8950390f6447a9ed3cfe6f6
DavidE1025 <DavidEmory1025@gmail.com>
2016-02-18 02:03:25 -0800

    Final
    All questions are working Except for number 4. I will come back and
    check it later.

--- Lab 4.sql
+++ Lab 4.sql
@@ -31,10 +31,10 @@ WHERE cid IN (SELECT cid
 SELECT cid
 FROM customers
 WHERE cid IN (SELECT cid
-	      FROM orders
+	      FROM orders 
 	      WHERE pid IN (SELECT pid
 			    FROM products
-			    WHERE pid = 'p01' AND pid = 'p07'));
+			    WHERE pid = 'p01' AND pid = 'p07')); 
 --5. Get the ids of products not ordered by any customers who placed any order through agent a07 in pid order from highest to lowest 
 SELECT pid
 FROM products
@@ -45,5 +45,22 @@ WHERE pid IN (SELECT pid
 			    WHERE aid != 'a07'))
 ORDER BY pid DESC;
 --6. Get the name, discounts, and city for all customers who placed orders through agents in London or new York.
+SELECT name, discount, city
+FROM customers
+WHERE cid IN (SELECT cid
+	      FROM orders
+	      WHERE aid IN (SELECT aid 
+			    FROM agents
+			    WHERE city = 'London' OR city = 'New York'));
 --7. Get all customers who have the same discount as that of any customers in Dallas or London 
---8. Lot of typing (Dave is cool) 
+SELECT name
+FROM customers
+WHERE discount IN (SELECT discount
+		   FROM customers
+		   WHERE city = 'Dallas' OR city = 'London');
+--8. Check Constraints 
+-- Check constraints prevent people from inserting data into a table that should not be in it. 
+-- A good use of a check constraint is for a name field. 
+-- The constraint would prevent a user from putting in a number into the input field.
+-- Another constraint would prevent a user from leaving the name feild empty
+-- If a feild is vital for a database or system using the database a check constraint should be used.
