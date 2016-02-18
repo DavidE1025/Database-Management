493fe41f90c7128437e60d25c2c18847aebb38b4
DavidE1025 <DavidEmory1025@gmail.com>
2016-02-17 21:01:10 -0800

    Queries
    Queries 4 & 5

--- Lab 4.sql
+++ Lab 4.sql
@@ -17,7 +17,8 @@ WHERE pid IN (SELECT pid
 	      FROM orders
 	      WHERE cid IN (SELECT cid 
 			     FROM customers
-			     WHERE city = 'Dallas'));
+			     WHERE city = 'Dallas'))
+ORDER BY pid DESC;
 --3. Get the ids and names of customers who did not place an order through agent a01
 SELECT cid, name
 FROM customers
@@ -29,7 +30,20 @@ WHERE cid IN (SELECT cid
 --4. Get the ids of customers who ordered both product p01 and p07
 SELECT cid
 FROM customers
+WHERE cid IN (SELECT cid
+	      FROM orders
+	      WHERE pid IN (SELECT pid
+			    FROM products
+			    WHERE pid = 'p01' AND pid = 'p07'));
 --5. Get the ids of products not ordered by any customers who placed any order through agent a07 in pid order from highest to lowest 
+SELECT pid
+FROM products
+WHERE pid IN (SELECT pid
+	      FROM orders
+	      WHERE aid IN (SELECT aid
+			    FROM agents
+			    WHERE aid != 'a07'))
+ORDER BY pid DESC;
 --6. Get the name, discounts, and city for all customers who placed orders through agents in London or new York.
 --7. Get all customers who have the same discount as that of any customers in Dallas or London 
---8. Lot of typing (Dave is cool)  
+--8. Lot of typing (Dave is cool) 
