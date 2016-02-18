ab04c78704cef84ed5396ae8b62c846fd1a713b0
DavidE1025 <DavidEmory1025@gmail.com>
2016-02-17 20:29:59 -0800

    Queries
    Queries 1-3 done

--- Lab 4.sql
+++ Lab 4.sql
@@ -3,10 +3,33 @@
 --Lab 4 Sub-Queries
 
 --1. Get the cities of agents booking an order for a customer whos cid is 'c002'
+SELECT city
+FROM agents
+WHERE  aid IN (SELECT aid
+	       FROM orders
+	       WHERE cid IN (SELECT cid
+			     FROM customers
+			     WHERE cid = 'c002'));
 --2. Get the ids of products ordered through any agent who takes atleast one order from a customer in Dallas, sorted by pid from highest to lowest 
---3. Get the ids and names of customers who di not place an order through agent a01
+SELECT pid
+FROM products
+WHERE pid IN (SELECT pid
+	      FROM orders
+	      WHERE cid IN (SELECT cid 
+			     FROM customers
+			     WHERE city = 'Dallas'));
+--3. Get the ids and names of customers who did not place an order through agent a01
+SELECT cid, name
+FROM customers
+WHERE cid IN (SELECT cid
+	      FROM orders
+	      WHERE aid IN (SELECT aid
+			    FROM agents
+			    WHERE aid != 'a01'));
 --4. Get the ids of customers who ordered both product p01 and p07
+SELECT cid
+FROM customers
 --5. Get the ids of products not ordered by any customers who placed any order through agent a07 in pid order from highest to lowest 
 --6. Get the name, discounts, and city for all customers who placed orders through agents in London or new York.
 --7. Get all customers who have the same discount as that of any customers in Dallas or London 
---8. Lot of typing (Dave is cool) 
+--8. Lot of typing (Dave is cool)  
