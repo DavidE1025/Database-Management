ff10c4e7d46da1b38fa8a0a412a047399efe48f9
DavidE1025 <DavidEmory1025@gmail.com>
2016-02-21 20:18:02 -0800

    Lab 5
    Name and questions have been added.

---
+++ Lab 5.sql
@@ -0,0 +1,11 @@
+﻿--David Emory
+--Lab 5
+--2/24/2016
+
+--1.) Show the cities of agents booking an order for a customer whose id is 'c002'. Use joins; no sub queries
+--2.) Show the ids of products ordered through any agent who makes at least one order for a customer in Dallas, sorted by pid from highest to lowest. Use joins; no subqueries
+--3.) Show the names of customers who have never placed an order. Use a subquery
+--4.) Show the names of customers who have never placed an order. Use an outer join
+--5.) Show the names of customers who placed at least one order through an agent in their own city, along with those agent(s') names.
+--6.) Show the names of customers and agents living in the same city, along with the name of the shared city, regardless of whether or not the customer has ever placed an order with that agent
+--7.) Show th ename of customers who live in the city thet makes the fewest different kinds of products.
